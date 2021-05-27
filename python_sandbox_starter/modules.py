85edcc8534412c6659781114a33b310415e48b8f
nitish <nitish.singh158@gmail.com>
2021-05-26 23:03:50 -0600

    added tuples files

--- python_sandbox_starter/modules.py
+++ python_sandbox_starter/modules.py
@@ -1,1 +1,6 @@
 # A module is basically a file containing a set of functions to include in your application. There are core python modules, modules you can install using the pip package manager (including Django) as well as custom modules
+import datetime
+
+print(datetime.date.today())
+# now = datetime.now()
+# print(now)
--- python_sandbox_starter/tuples_sets.py
+++ python_sandbox_starter/tuples_sets.py
@@ -3,3 +3,9 @@
 
 
 # A Set is a collection which is unordered and unindexed. No duplicate members.
+fruit_set = {'Apple', 'Mango', 'Orange'}
+fruit_set.add('Grape')
+fruit_set.remove('Grape')
+fruit_set.clear()
+del fruit_set
+print(fruit_set)
